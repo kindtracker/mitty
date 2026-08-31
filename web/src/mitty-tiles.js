11432d351ec659a86f75c451cd56695f823702be
0rndm_p <277301021+0rndm@users.noreply.github.com>
2026-08-31 20:15:33 -0300

    fix: fix breaking animation

--- web/src/mitty-tiles.js
+++ web/src/mitty-tiles.js
@@ -4,24 +4,24 @@ window.mtiles = {
     hardness: 0.2
   },
   "mitty:breaking:1": {
-    pos: [12, 1]
+    pos: [0, 8]
   },
   "mitty:breaking:2": {
-    pos: [12, 2]
+    pos: [1, 8]
   },
   "mitty:breaking:3": {
-    pos: [12, 3]
+    pos: [2, 8]
   },
   "mitty:breaking:4": {
-    pos: [12, 4]
+    pos: [3, 8]
   },
   "mitty:breaking:5": {
-    pos: [12, 5]
+    pos: [4, 8]
   },
   "mitty:breaking:6": {
-    pos: [12, 6]
+    pos: [5, 8]
   },
   "mitty:breaking:7": {
-    pos: [12, 7]
+    pos: [6, 8]
   }
 }
